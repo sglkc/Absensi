ce9b79e4676d26c2117bce9b8d2a7710eb7f4ed5
sglkc <31957516+sglkc@users.noreply.github.com>
2021-03-10 11:22:14 +0700

    Comment

--- mysql.php
+++ mysql.php
@@ -1,10 +1,13 @@
 <?php
+// Nama untuk hapus data
 $adminName = "password";
+
+// Database
 $user = "root";
 $pass = "";
 $server = "localhost";
-$database = "absensi";
-$dbTable = "siswa";
+$database = "absensi"; // Nama database
+$dbTable = "siswa"; // Nama tabel
 $mysqli = new mysqli($server, $user, $pass, $database);
 
 if(!$mysqli) die("Fail:<br>" . mysqli_connect_error());
